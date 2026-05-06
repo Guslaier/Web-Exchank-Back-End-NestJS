59d3a6fc59d15dbb8aed73349aaec7daf02bd1d3
XunziTH <pop3king@gmail.com>
2026-05-06 19:30:58 +0700

    feat(report) : controlerr Get previous/shift.

--- src/modules/reports/reports.controller.ts
+++ src/modules/reports/reports.controller.ts
@@ -1,18 +1,24 @@
-import { Controller , UseGuards , Put, Param, Body } from "@nestjs/common";
+import { Controller , UseGuards , Param  , Query , Body , Put , Get } from "@nestjs/common";
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { Roles } from '../../common/decorators/roles.decorator'; 
 import { CurrentUser } from "../../common/decorators/current-user.decorator";
-import { PutShiftBody } from './dto/report.dto'
+import { GetPreviousShift , PutShiftBody } from './dto/report.dto'
+import { ReportsService } from './reports.service'  ;
+import { report } from "process";
 
 @Controller('reports') 
 export class ReportsController {
 
+    constructor(
+        private readonly reportService : ReportsService ,
+    ){}
+
     @UseGuards(JwtAuthGuard , RolesGuard)
     @Roles('ADMIN' , 'MANAGER')
-    @Put('shift/')
-    PutShift(@CurrentUser() user : any , @Param() param : any , @Body() body : PutShiftBody) {
-
+    @Get('previous/shift')
+    getPreviousShfit(@CurrentUser() user : any , @Query() query : GetPreviousShift) {
+        return this.reportService.getPreviousShiftData(user , query.id) ; 
     }
 
 }
